76dfe3a952b4af497ff95d2b32691fae430e4db5
Corazor Technology Private Limited <153815210+Corazortechnology@users.noreply.github.com>
2025-01-30 14:38:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,30 @@
 //   },
 //   plugins: [react()],
 // });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/', // Ensures that the application works with Vercel routing
+//   build: {
+//     chunkSizeWarningLimit: 1600,
+//     outDir: "dist",
+//   },
+//   plugins: [react()],
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Ensures that the application works with Vercel routing
+  base: '/', // Ensures correct routing
+  server: {
+    port: 8080, // Ensure Azure App Service runs on the correct port
+    host: "0.0.0.0", // Allows external access in Azure environment
+    strictPort: true, // Ensures Vite doesn't fallback to another port
+  },
   build: {
     chunkSizeWarningLimit: 1600,
     outDir: "dist",
